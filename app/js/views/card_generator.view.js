3ba790401d59ba7b2efc97526fce6f750e19a45f
Joshua Comeau <joshwcomeau@gmail.com>
2015-04-12 21:45:40 -0400

    Successfully fetching data from NASA through our express proxy server.

--- app/js/views/card_generator.view.js
+++ app/js/views/card_generator.view.js
@@ -1,19 +1,26 @@
 GreetingsFromSpace.Views.cardGenerator = Backbone.View.extend({
   el: "#content",
   initialize: function() {
-    console.log("cardGenerator initialized")
     this.render();
   },
   render: function() {
     var template;
 
-    console.log("Rendering content");
-    console.log("This:", this);
+    // Get our photo of the day
+    $.ajax({
+      url: '/api/nasa', 
+      dataType: 'json',
+      success: function(rawNasaData) {
+        var nasaData = JSON.parse(rawNasaData);
 
-    $.get( '/templates/card_generator.template.html', function(data) {
-      console.log("Fetched ", data);
-      template = _.template(data, { name: 'Josh' });
-      this.$el.html(template);
-    }.bind(this), 'html')
+        $.get( '/templates/card_generator.template.html', function(templateData) {
+          template = _.template(templateData, { photoUrl: nasaData.url });
+          this.$el.html(template);
+        }.bind(this), 'html');
+
+      }.bind(this)
+    });
+
+    
   }
 });
--- routes.js
+++ routes.js
@@ -1,18 +1,35 @@
-var path = require('path');
+var path    = require('path'),
+    request = require('request');
 
 
-
+var API_KEY   = 'caivMvUMm1x4HLA2z12d7yUvw7NLpaQDeDhKGhPg';
+var nasa_url  = 'https://api.data.gov/nasa/planetary/apod?date=2015-04-12&api_key='+API_KEY;
 
 module.exports = function(app) {
   // Server routes =========================================
   // Handles things like API calls and authentication
+
+  app.get('/api/nasa', function(req, res) {
+
+    request(nasa_url, function(error, nasaRes, body) {
+      res.json(body);
+    });
+
+    // console.log("GET /api/nasa");
+    // $.ajax({
+    //   url: nasa_url, 
+    //   dataType: 'json',
+    //   success: function(nasaData) {
+    //   }.bind(this)
+    // });
+
+  });
 
 
   // Client routes =========================================
 
   // Backbone will handle all front-end routes, SPA-style. Just send it there.
   app.get('*', function(req, res) {
-    console.log('request', path.join(__dirname, '/app'));
     res.sendFile('index.html', { root: path.join(__dirname, './app') });
   });
 };
